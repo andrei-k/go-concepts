359a99c1029a53165bf27f2b114cf4e9b16d415a
Andrei K <andrei@12ak.com>
2022-04-03 13:51:06 -0400

    Function to ask for age

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -11,7 +12,9 @@ var reader *bufio.Reader
 
 func main() {
 	userName := readString("What is your name?")
-	fmt.Println("Your name is", userName)
+	userAge := readInt("How old are you?")
+
+	fmt.Println("Your name is", userName, "and your age", userAge)
 }
 
 func prompt() {
@@ -26,8 +29,26 @@ func readString(s string) string {
 	userInput, _ := reader.ReadString('\n')
 	// Strip off the carriage return (Windows)
 	userInput = strings.Replace(userInput, "\r\n", "", -1)
-	// (Mac)
 	userInput = strings.Replace(userInput, "\n", "", -1)
 
 	return userInput
+}
+
+func readInt(s string) int {
+	for {
+		fmt.Println(s)
+		prompt()
+
+		userInput, _ := reader.ReadString('\n')
+		userInput = strings.Replace(userInput, "\r\n", "", -1)
+		userInput = strings.Replace(userInput, "\n", "", -1)
+
+		num, err := strconv.Atoi(userInput)
+		if err != nil {
+			fmt.Println("Please enter a whole number")
+		} else {
+			return num
+		}
+
+	}
 }
