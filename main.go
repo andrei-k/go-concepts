554fc1ea59430c0eed4723534161204e2b9b2fce
Andrei K <andrei@12ak.com>
2022-04-03 08:03:36 -0400

    Set up bufio.NewReader

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("What is your name?")
+	fmt.Print("->")
+
+	userInput, _ := reader.ReadString('\n')
+
+	// Strip off the carriage return (Windows)
+	userInput = strings.Replace(userInput, "\r\n", "", -1)
+	// (Mac)
+	userInput = strings.Replace(userInput, "\n", "", -1)
+
+	fmt.Println("Your name is", userInput)
+}
