9852026044035084197f6b554917e7f46a96011a
Andrei K <andrei@12ak.com>
2022-04-03 08:13:41 -0400

    Create readString func

--- main.go
+++ main.go
@@ -7,21 +7,27 @@ import (
 	"strings"
 )
 
+var reader *bufio.Reader
+
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("What is your name?")
+	userName := readString("What is your name?")
+	fmt.Println("Your name is", userName)
+}
+
+func prompt() {
+	fmt.Print("-> ")
+}
+
+func readString(s string) string {
+	fmt.Println(s)
 	prompt()
 
+	reader = bufio.NewReader(os.Stdin)
 	userInput, _ := reader.ReadString('\n')
-
 	// Strip off the carriage return (Windows)
 	userInput = strings.Replace(userInput, "\r\n", "", -1)
 	// (Mac)
 	userInput = strings.Replace(userInput, "\n", "", -1)
 
-	fmt.Println("Your name is", userInput)
-}
-
-func prompt() {
-	fmt.Print("-> ")
+	return userInput
 }
