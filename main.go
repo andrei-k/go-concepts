201265c8f555bb17255d8213a851b12ef5b4e0a6
Andrei K <andrei@12ak.com>
2022-04-03 14:00:29 -0400

    Use Printf to print string with variables

--- main.go
+++ main.go
@@ -14,7 +14,9 @@ func main() {
 	userName := readString("What is your name?")
 	userAge := readInt("How old are you?")
 
-	fmt.Println("Your name is", userName, "and your age", userAge, "years old.")
+	// fmt.Println("Your name is"+userName+". You are age ", userAge, "years old.")
+	// fmt.Println(fmt.Sprintf("Your name is %s. You are %d years old", userName, userAge))
+	fmt.Printf("Your name is %s. You are %d years old.\n", userName, userAge)
 }
 
 func prompt() {
