1ff924bacbc7050ef1bfb866f6e578e412495e25
Andrei K <andrei@12ak.com>
2022-04-04 21:45:15 -0400

    Use the keyboard package

--- main.go
+++ main.go
@@ -3,16 +3,21 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 	"strings"
+
+	"github.com/eiannone/keyboard"
 )
+
+// Install with" go get -u github.com/eiannone/keyboard
 
 var reader *bufio.Reader
 
 type User struct {
-	UserName string
-	UserAge  int
+	Name     string
+	Age      int
 	FaveNum  float64
 	OwnsADog bool
 }
@@ -22,14 +27,14 @@ func main() {
 
 	var user User
 
-	user.UserName = readString("What is your name?")
-	user.UserAge = readInt("How old are you?")
+	user.Name = readString("What is your name?")
+	user.Age = readInt("How old are you?")
 	user.FaveNum = readFloat("What is your favourite number?")
-	user.OwnsADog = readBool("Do you own a dog? (y/n)")
+	user.OwnsADog = readBool2("Do you own a dog? (y/n)")
 
 	fmt.Printf("Your name is %s. You are %d years old. Your favourite number is %.3f. Owns a dog: %t\n",
-		user.UserName,
-		user.UserAge,
+		user.Name,
+		user.Age,
 		user.FaveNum,
 		user.OwnsADog,
 	)
@@ -103,13 +108,41 @@ func readBool(s string) bool {
 		userInput = strings.Replace(userInput, "\n", "", -1)
 
 		if userInput == "" || (userInput != "y" && userInput != "n") {
-			fmt.Println("Please enter y or n")
+			fmt.Println("Please type y or n")
 		} else {
 			if userInput == "y" {
 				return true
 			} else {
 				return false
 			}
+		}
+	}
+}
+
+// Using the keyboard package
+func readBool2(s string) bool {
+	err := keyboard.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		_ = keyboard.Close()
+	}()
+
+	for {
+		fmt.Println(s)
+		char, _, err := keyboard.GetSingleKey()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if strings.ToLower(string(char)) != "y" && strings.ToLower(string(char)) != "n" {
+			fmt.Println("Please type y or n")
+		} else if char == 'n' || char == 'N' {
+			return false
+		} else if char == 'y' || char == 'Y' {
+			return true
 		}
 	}
 }
